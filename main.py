a02ba10f115dcff99df93648579d175c4a6f182f
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-04-24 15:02:05 +0200

    Just a mise-en-bouche

---
+++ main.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+# Reading the input file
+filename = ''
+f = open(filename, 'r')
+total_population = f.read().split('')
+f.close()
+
+# Generating random population
+
+# Fitness function
+
+
+def fitness(individual):
+    pass
+
+# Crossover
+
+
+def crossover(individual_A, individual_B):
+    pass
+
+# Mutation
+
+
+def mutation(individual, population=total_population):
+    pass
+
+# Roulette wheel selection
+
+
+def selection(population):
+    pass
